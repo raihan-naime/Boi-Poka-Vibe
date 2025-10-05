c8562989c8c267f9d87456f3160bafe2266479a9
RaihanNaim <imraihan.naim@gmail.com>
2025-10-06 00:09:30 +0600

    fetch data through useEffect

---
+++ src/Pages/Books/Books.jsx
@@ -0,0 +1,18 @@
+import React, { use, useEffect, useState } from 'react';
+
+const Books = () => {
+    const [allBokks, setAllBooks] = useState([])
+    useEffect(()=>{
+        fetch('booksData.json')
+        .then(res => res.json())
+        .then(data => setAllBooks(data)
+        )
+    }, [])
+    return (
+        <div>
+            <h1>books</h1>
+        </div>
+    );
+};
+
+export default Books;
